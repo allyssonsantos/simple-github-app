441e4601d254a1ab1feb9dcba2b6129bbebf5206
Allysson Santos <aallysson0@gmail.com>
2019-02-24 10:38:40 -0300

    Create Container Component and apply css grid

--- components/RepoCard/index.js
+++ components/RepoCard/index.js
@@ -3,12 +3,10 @@ import styled from 'styled-components';
 
 import { Header, Content, Footer } from './sub-components';
 
-const StyledCard = styled.section`
+const StyledCard = styled.div`
   background-color: #fff;
   border-radius: 3px;
   box-shadow: 0px 0px 1px #000;
-  max-width: 300px;
-  min-height: 300px;
 `;
 
 class RepoCard extends Component {
--- pages/user.js
+++ pages/user.js
@@ -1,42 +1,51 @@
 import React, { Component } from 'react';
-
-import Form from '../components/Form';
+import styled from 'styled-components';
 
 import { getUserData } from '../services/githubApi';
 import { AppConsumer } from '../providers/AppProvider';
 
+import Container from '../components/Container';
+import Form from '../components/Form';
 import Title from '../components/Title';
 import RepoCard from '../components/RepoCard';
-import RightPanel from '../components/RightPanel';
+
+const Repos = styled.main`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-gap: 10px;
+`;
 
 class User extends Component {
   render() {
     return (
-      <AppConsumer>
-        {context => {
-          console.log(context);
-          const {
-            userInfo: { name },
-            repos,
-          } = context;
-          return (
-            <>
-              <Form />
-              <Title>{name}</Title>
-              {repos.map(repo => {
-                return (
-                  <RepoCard key={repo.id}>
-                    <RepoCard.Header />
-                    <RepoCard.Content />
-                    <RepoCard.Footer />
-                  </RepoCard>
-                );
-              })}
-              <RightPanel />
-            </>
-          );
-        }}
-      </AppConsumer>
+      <Container>
+        <AppConsumer>
+          {context => {
+            console.log(context);
+            const {
+              userInfo: { name },
+              repos,
+            } = context;
+            return (
+              <>
+                <Form />
+                <Title>{name}</Title>
+                <Repos>
+                  {repos.map(repo => {
+                    return (
+                      <RepoCard key={repo.id}>
+                        <RepoCard.Header />
+                        <RepoCard.Content />
+                        <RepoCard.Footer />
+                      </RepoCard>
+                    );
+                  })}
+                </Repos>
+              </>
+            );
+          }}
+        </AppConsumer>
+      </Container>
     );
   }
 }
