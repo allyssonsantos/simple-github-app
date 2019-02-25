8f508fc87a2f66eb65e5d18f4529393c9d612c6e
Allysson Santos <aallysson0@gmail.com>
2019-02-24 21:32:35 -0300

    Remove button component

--- components/Button/index.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const StyledButton = styled.button``;
-
-const Button = ({ children }) => <StyledButton>{children}</StyledButton>;
-
-export default Button;
